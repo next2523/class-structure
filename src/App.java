119c0de25483a45eee613314088f832b9d637765
next2523 <70048830+next2523@users.noreply.github.com>
2023-08-10 19:21:49 -0400

    created a subclass and changed some stuff

--- src/App.java
+++ src/App.java
@@ -1,11 +1,27 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Dog Hilary = new Dog("Curly", 69, "Blue", "Hilary");
-        Dog Hayden = new Dog("Straight", 4, "Brown", "Hayden");
-        Dog Levi = new Dog("Slightly Curly", 15, "Orange", "Levi");
+        Dog Hilary = new Dog("Curly", 69, "Blue", "Hilary", 80);
+        Dog Hayden = new Dog("Straight", 4, "Brown", "Hayden", 30);
+        Dog Levi = new Dog("Slightly Curly", 15, "Orange", "Levi", 167);
 
-        System.out.println(Hilary.getDog() + "\n");
-        System.out.println(Hayden.getDog() + "\n");
-        System.out.println(Levi.getDog() + "\n");
+        goldenRetriever GoldenHilary = new goldenRetriever("Straight", 17, "Yellow", "Max", 267);
+        goldenRetriever GoldenHayden = new goldenRetriever("Straight", 17, "Yellow", "Max", 267);
+        goldenRetriever GoldenLevi = new goldenRetriever("Straight", 17, "Yellow", "Max", 267);
+
+        // System.out.println(Hilary.getDog() + "\n");
+        // System.out.println(Hayden.getDog() + "\n");
+        // System.out.println(Levi.getDog() + "\n");
+
+        // System.out.println(Hilary.nameAndAge());
+        // System.out.println(Hayden.nameAndAge());
+        // System.out.println(Levi.nameAndAge());
+
+        // System.out.println(Hilary.lifeExpectancy());
+        // System.out.println(Hayden.lifeExpectancy());
+        // System.out.println(Levi.lifeExpectancy());
+
+        // System.out.println(GoldenHayden.getGoldenRetriever());
+        // System.out.println(GoldenHilary.getGoldenRetriever());
+        // System.out.println(GoldenLevi.getGoldenRetriever());
     }
 }
